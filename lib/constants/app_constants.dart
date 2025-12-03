a9c9358a819dcea6c42a9663f20f5f278e135930
hyago <hyagophilip@gmail.com>
2025-12-03 13:12:34 -0300

    Feat: Adiciona constante para AdMob ID

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,7 @@
+
+class AdConfig {
+  static const String adUnitId = 'ca-app-pub-2109984465311613/4646382083';
+  // ID de teste para DEV
+  static const String testBannerAdUnitId = 'ca-app-pub-3940256099942544/6300978111';
+
+}
